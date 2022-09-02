eec365307dbc9fb7bda18433549bdc5d4faf8581
Masaki Murooka <masaki.murooka@gmail.com>
2022-09-02 14:56:37 +0900

    Wait until the omniorb log file is generated in clear-omninames.sh

--- projects/JVRC1/cnoid/clear-omninames.sh
+++ projects/JVRC1/cnoid/clear-omninames.sh
@@ -5,5 +5,8 @@ sudo rm -f /var/log/omniorb-nameserver.log 2> /dev/null
 sudo rm -f /var/lib/omniorb/* 2> /dev/null
 sudo rm -f /tmp/rtcmanager.ref 2> /dev/null
 sudo /etc/init.d/omniorb4-nameserver start
-sleep 1
+until [ -f /var/log/omniorb-nameserver.log ]
+do
+     sleep 1
+done
 tail /var/log/omniorb-nameserver.log
