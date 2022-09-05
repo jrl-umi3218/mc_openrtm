953227e190371965f79ed850259bb0f1afcf2fbb
Masaki Murooka <m-murooka@aist.go.jp>
2022-09-05 09:32:58 +0900

    Wait 10 iterations at most in clear-omninames.sh

--- projects/JVRC1/cnoid/clear-omninames.sh
+++ projects/JVRC1/cnoid/clear-omninames.sh
@@ -5,8 +5,9 @@ sudo rm -f /var/log/omniorb-nameserver.log 2> /dev/null
 sudo rm -f /var/lib/omniorb/* 2> /dev/null
 sudo rm -f /tmp/rtcmanager.ref 2> /dev/null
 sudo /etc/init.d/omniorb4-nameserver start
-until [ -f /var/log/omniorb-nameserver.log ]
+for i in {0..9}
 do
-     sleep 1
+    [ -f /var/log/omniorb-nameserver.log ] && break
+    sleep 1
 done
 tail /var/log/omniorb-nameserver.log
